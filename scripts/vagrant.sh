0b8c5d47ab17abb091590bd634e6473dd8903ec6
Lewis Daly <lewisdaly@me.com>
2020-08-14 12:01:55 +0800

    hack: disable snap and all setup for clean ubuntu image

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -40,38 +40,38 @@ echo "Mojaloop: run update ..."
 apt update
 
 echo "Mojaloop: installing snapd ..."
-apt install snapd -y
+# apt install snapd -y
 
-echo "Mojaloop: installing microk8s release $KUBERNETES_RELEASE ... "
-sudo snap install microk8s --classic --channel=$KUBERNETES_RELEASE/stable
+# echo "Mojaloop: installing microk8s release $KUBERNETES_RELEASE ... "
+# sudo snap install microk8s --classic --channel=$KUBERNETES_RELEASE/stable
 
-echo "Mojaloop: enable helm ... "
-microk8s.enable helm3 
-echo "Mojaloop: enable dns ... "
-microk8s.enable dns
-echo "Mojaloop: enable storage ... "
-microk8s.enable storage
-echo "Mojaloop: enable ingress ... "
-microk8s.enable ingress
-echo "Mojaloop: install postman ..."   
-sudo snap install postman
+# echo "Mojaloop: enable helm ... "
+# microk8s.enable helm3 
+# echo "Mojaloop: enable dns ... "
+# microk8s.enable dns
+# echo "Mojaloop: enable storage ... "
+# microk8s.enable storage
+# echo "Mojaloop: enable ingress ... "
+# microk8s.enable ingress
+# echo "Mojaloop: install postman ..."   
+# sudo snap install postman
 
-echo "Mojaloop: add convenient aliases..." 
-snap alias microk8s.kubectl kubectl
-snap alias microk8s.helm3 helm
+# echo "Mojaloop: add convenient aliases..." 
+# snap alias microk8s.kubectl kubectl
+# snap alias microk8s.helm3 helm
 
-echo "Mojaloop: add vagrant user to microk8s group"
-usermod -a -G microk8s vagrant
-#chown -f -R vagrant ~vagrant/.kube
+# echo "Mojaloop: add vagrant user to microk8s group"
+# usermod -a -G microk8s vagrant
+# #chown -f -R vagrant ~vagrant/.kube
 
-echo "Mojaloop: add repos and deploy helm charts ..." 
-su - vagrant -c "microk8s.helm3 repo add mojaloop http://mojaloop.io/helm/repo/"
-su - vagrant -c "microk8s.helm3 repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator"
-su - vagrant -c "microk8s.helm3 repo add kiwigrid https://kiwigrid.github.io"
-su - vagrant -c "microk8s.helm3 repo add elastic https://helm.elastic.co"
-su - vagrant -c "microk8s.helm3 repo update"
-su - vagrant -c "microk8s.helm3 list"
-su - vagrant -c "microk8s.helm3 repo list"
+# echo "Mojaloop: add repos and deploy helm charts ..." 
+# su - vagrant -c "microk8s.helm3 repo add mojaloop http://mojaloop.io/helm/repo/"
+# su - vagrant -c "microk8s.helm3 repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator"
+# su - vagrant -c "microk8s.helm3 repo add kiwigrid https://kiwigrid.github.io"
+# su - vagrant -c "microk8s.helm3 repo add elastic https://helm.elastic.co"
+# su - vagrant -c "microk8s.helm3 repo update"
+# su - vagrant -c "microk8s.helm3 list"
+# su - vagrant -c "microk8s.helm3 repo list"
     
 #echo "MojaLoop: Deploy mojaloop" 
 # Note troubleshooting guide and the need for updated values.yml
