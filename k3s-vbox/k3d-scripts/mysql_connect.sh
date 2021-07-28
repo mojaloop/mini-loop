e863fc14ae74bd91b684af78207f55b359a0a37d
tom <tom.daly@oracle.com>
2021-07-28 11:14:49 +0930

    debugging

--- k3s-vbox/k3d-scripts/mysql_connect.sh
+++ k3s-vbox/k3d-scripts/mysql_connect.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # test mysql db connection
-kubectl run -it --rm --image=mysql:5.6 --restart=Never mysql -- mysql -h mysql-als -u account_lookup -ppassword account_lookup -e 
+#kubectl run -it --rm --image=mysql:5.6 --restart=Never mysql -- mysql -h mysql-als -u account_lookup -ppassword account_lookup -e 
+
+
+
+kubectl run -it --rm --image=mysql:5.6 --restart=Never mysql -- mysql -h mysql-cl -P 3306 -u central_ledger --password=password  central_ledger   -e 'select version()'
