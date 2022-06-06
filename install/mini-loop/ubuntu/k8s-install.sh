5bda78c20065085c0a5713ed785c2db4212b8442
tdaly61 <tdaly61@gmail.com>
2022-06-06 08:35:49 +0000

    almost working k3s support

--- install/mini-loop/ubuntu/k8s-install.sh
+++ install/mini-loop/ubuntu/k8s-install.sh
@@ -20,21 +20,46 @@ function check_pi {
     fi    
 }
 
+function print_ok_oses {
+    printf "      Fedora versions: " 
+    for i in "${FEDORA_OK_VERSIONS_LIST[@]}"; do
+        printf " [%s]" "$i"
+    done
+    printf "\n"
+    printf "      Ubuntu versions: " 
+    for i in "${UBUNTU_OK_VERSIONS_LIST[@]}"; do
+        printf " [%s]" "$i"
+    done
+    printf "\n"
+}
+
 function check_os_ok {
-    # check this is ubuntu OS and a recent version => all ok ; else warn user it is not well tested
+    printf " ==> check that this os and version is tested with mojaloop (mini-loop scripts)\n"
     ok=false
-    # make sure the utility to check the operating system type and version exists
+    # check for redhat family OS 
+    # TODO figure out what minimums to support here e.g. RHEL 8 Fedora 36 and Oracle 8 etc 
+    if [ -f /etc/redhat-release ] || [ -f /etc/fedora-release ]; then    
+        LINUX_OS=`cat /etc/redhat-release | cut -d " " -f1` 
+        if [[ "$LINUX_OS" == "Fedora" ]]; then 
+            ver=`cat /etc/redhat-release | cut -d " " -f3`
+            printf "     Linux Operating System is [Fedora] and version is [%s]\n" "$ver"
+            for i in "${FEDORA_OK_VERSIONS_LIST[@]}"; do
+                if  [[ "$ver" == "$i" ]]; then
+                     ok=true         
+                     break
+                fi  
+            done
+        fi
+    fi
+    # check for Ubuntu 
     if [ -x "/usr/bin/lsb_release" ]; then
-        # now check that this os is Ubuntu (for now this is all that is tested)
-        os=`lsb_release --d | perl -ne 'print  if s/^.*Ubuntu.*(\d+).(\d+).*$/Ubuntu/' `
-        if [[ $os == "Ubuntu" ]] ; then 
+        LINUX_OS=`lsb_release --d | perl -ne 'print  if s/^.*Ubuntu.*(\d+).(\d+).*$/Ubuntu/' `
+        if [[ $LINUX_OS == "Ubuntu" ]] ; then 
             printf "Identified operating system as %s [ok] \n" $os   
-            # now check that the Ubuntu version is reasonably recent 
             ver=`/usr/bin/lsb_release --d | perl -ne 'print $&  if m/(\d+)/' `
-            for i in "${OS_VERSIONS_LIST[@]}"; do
+            for i in "${UBUNTU_OK_VERSIONS_LIST[@]}"; do
                 if  [[ "$ver" == "$i" ]]; then
                      ok=true
-                     printf "Identified operating system release as %s [ok] \n" "$i" 
                      break
                 fi  
             done
@@ -42,28 +67,34 @@ function check_os_ok {
     fi
 
     if [[ "$ok" == "false" ]]; then 
-        printf "** Error:  either the operating system is not Ubuntu or \n" "$ok"
-        printf "   it is older than version 16 of Ubuntu or newer than version 20 \n" "$ok"
-        printf "   Currently this script is only well tested against versions of Ubuntu 16 to 20  ** \n "
-        printf "* Note: if you are confident you could proceed by editing this script and commenting out the check_os_ok test \n"
-        printf "   and then re-run but I have not tested it outside of recent Ubuntu releases * \n"
-        exit 1
+        if [[ $k8s_distro == "k3s" ]]; then 
+            printf " [?] \n"
+            printf "** Warning : k3s & Mojaloop not tested on this operating system and/or version \n" 
+            printf "             but it should work ok **\n"
+        else
+            printf "** Error : This operating system combination and/or version seems to be untested with mini-loop ** \n" 
+            printf "   Tested os types and versions are ... \n" 
+            print_ok_oses
+            exit 1
+        fi
+    else
+        printf "     os and version check  [ok] \n"
     fi 
-}
-
-
+} 
 
 
 function install_prerequisites {
-    printf "==> Install prerequisites: run update ...\n"
-    apt update
-
-    printf "==> Install prerequisites: installing snapd ...\n"
-    apt install snapd -y 
-
-    printf "==> Install prerequisites: installing python and python libs ...\n"
-    apt install python3-pip -y 
-    pip3 install ruamel.yaml
+    printf "==> Install any OS prerequisites , tools &  updates  ...\n"
+    if [[ $LINUX_OS == "Ubuntu" && $k8s_distro == "microk8s" ]]; then 
+        printf "   apt update \n"
+        apt update > /dev/null 2>&1
+        printf "   install snapd\n"
+        apt install snapd -y > /dev/null 2>&1
+        printf "    python and python libs ...\n"
+        apt install python3-pip -y 
+        pip3 install ruamel.yaml
+    fi 
+    # todo what about non ubuntu, still want python3 and ruamel 
 }
 
 function add_hosts {
@@ -83,8 +114,6 @@ function add_hosts {
     ping  -c 2 account-lookup-service-admin.local
 }
 
-
-
 function set_k8s_distro { 
     if [[ -z "$k8s_distro" ]]; then  
         k8s_distro=$DEFAULT_K8S_DISTRO
@@ -98,21 +127,32 @@ function set_k8s_distro {
             exit 1
         fi
     fi
-
 }
 
-function set_k8_version {
-    printf "========================================================================================\n"
-    printf "Mojaloop k8s install : set k8s version to install (default and minimum is 1.21) \n"
-    printf "========================================================================================\n\n"
-    if [[ "$k8s_version" == "1.21" ]]  ; then
-            printf  " k8s version set correctly to : %s\n" $k8s_version
-    else 
-            printf "Note -v flag not specified or invalid  => k8s version will use default:  %s \n" $DEFAULT_K8S_VERSION
-            k8s_version=$DEFAULT_K8S_VERSION
+function set_k8s_version {
+    # printf "========================================================================================\n"
+    # printf "For now microk8s kubernetes release is v1.20  \n"
+    # printf "        and  k3s kubernetes release is v1.21  \n"
+    # printf " this is hardcoded now due to some limitations in the Mojaloop helm charts              \n"
+    # printf "========================================================================================\n\n"
+    if [[ $k8s_distro == "k3s" ]]; then 
+        DEFAULT_K8S_VERSION="v1.21"
     fi
-}
 
+    if [[ $k8s_distro == "microk8s" ]]; then 
+        DEFAULT_K8S_VERSION="1.20"
+    fi
+
+    # printf "========================================================================================\n"
+    # printf "Mojaloop k8s install : set k8s version to install (default and minimum is 1.21) \n"
+    # printf "========================================================================================\n\n"
+    # if [[ "$k8s_version" == "1.21" && $k8s_distro == "k3s" ]]  ; then
+    #         printf  " k8s version set correctly to : %s\n" $k8s_version
+    # else 
+    #         printf "Note -v flag not specified or invalid  => k8s version will use default:  %s \n" $DEFAULT_K8S_VERSION
+    #         k8s_version=$DEFAULT_K8S_VERSION
+    # fi
+}
 
 function do_microk8s_install {
     # TODO : Microk8s can complain that This is insecure. Location: /var/snap/microk8s/2952/credentials/client.config
@@ -140,7 +180,6 @@ function do_microk8s_install {
     echo "==> Mojaloop: add $k8s_user user to microk8s group"
     usermod -a -G microk8s $k8s_user
     sudo chown -f -R $k8s_user ~/.kube
-
 }
 
 function do_k3s_install {
@@ -148,28 +187,32 @@ function do_k3s_install {
     printf "Mojaloop k3s install : Installing Kubernetes k3s engine and tools (helm/ingress etc) \n"
     printf "========================================================================================\n"
 
-    # # first do docker (this can go once the percona chart issue is resolved )
-    # if [[ ! -f "/usr/bin/docker" ]]; then 
-    #     curl https://releases.rancher.com/install-docker/19.03.sh | sh
-    # fi 
-    # printf "=> creating docker group, adding user and restarting docker \n"
-    # groupadd docker > /dev/null 2>&1
-    # usermod -a -G docker $k8s_user > /dev/null 2>&1
-    # systemctl restart docker > /dev/null 2>&1
+    # first do docker (this can go once the percona chart issue is resolved )
+    if [[ ! -f "/usr/bin/docker" ]]; then 
+        curl https://releases.rancher.com/install-docker/19.03.sh | sh
+    fi 
+    printf "=> creating docker group, adding user and restarting docker \n"
+    groupadd docker > /dev/null 2>&1
+    usermod -a -G docker $k8s_user > /dev/null 2>&1
+    systemctl restart docker > /dev/null 2>&1
 
-    # # install k3s with docker 
-    # printf "=> installing k3s using  docker\n"
+    # install k3s with docker 
+    printf "=> installing k3s using  docker\n"
+    echo $DEFAULT_K8S_VERSION
+    curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" \
+                               INSTALL_K3S_CHANNEL=$DEFAULT_K8S_VERSION \
+                               INSTALL_K3S_EXEC=" --no-deploy traefik --docker " sh 
     # curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" \
     #                            INSTALL_K3S_CHANNEL=$K8S_VERSION \
     #                            INSTALL_K3S_EXEC=" --no-deploy traefik --docker " sh 
 
-    # export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-    # cp /etc/rancher/k3s/k3s.yaml  $k8s_user_home/k3s.yaml
-    # chown $k8s_user  $k8s_user_home/k3s.yaml
-    # chmod 600  $k8s_user_home/k3s.yaml 
-    # echo "source .bashrc" >>   $k8s_user_home/.bash_profile 
-    # echo "export KUBECONFIG=\$HOME/k3s.yaml" >>  $k8s_user_home/.bashrc
-    # echo "export KUBECONFIG=\$HOME/k3s.yaml" >>   $k8s_user_home/.bash_profile  
+    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+    cp /etc/rancher/k3s/k3s.yaml  $k8s_user_home/k3s.yaml
+    chown $k8s_user  $k8s_user_home/k3s.yaml
+    chmod 600  $k8s_user_home/k3s.yaml 
+    echo "source .bashrc" >>   $k8s_user_home/.bash_profile 
+    echo "export KUBECONFIG=\$HOME/k3s.yaml" >>  $k8s_user_home/.bashrc
+    echo "export KUBECONFIG=\$HOME/k3s.yaml" >>   $k8s_user_home/.bash_profile  
 
     # install helm
     printf "==> installing helm " 
@@ -180,11 +223,12 @@ function do_k3s_install {
         printf "** Error need to implement arm architecture install for helm ** \n"
         exit 1
     fi
-    cd /tmp
-    curl -L -s -o ./helm.tar.gz https://get.helm.sh/helm-v$HELM_VERSION-linux-$helm_arch_str.tar.gz
-    exit
-    cat ./helm.tar.gz | gzip -d -c | tar xf -
-    mv ./linux-amd64/helm /usr/local/bin  
+    rm -rf /tmp/linux-$helm_arch_str /tmp/helm.tar
+    curl -L -s -o /tmp/helm.tar.gz https://get.helm.sh/helm-v$HELM_VERSION-linux-$helm_arch_str.tar.gz
+    gzip -d /tmp/helm.tar.gz 
+    tar xf  /tmp/helm.tar -C /tmp
+    mv /tmp/linux-amd64/helm /usr/local/bin  
+    rm -rf /tmp/linux-$helm_arch_str
     /usr/local/bin/helm version > /dev/null 2>&1
     if [[ $? -eq 0 ]]; then 
         printf "[ok]\n"
@@ -209,23 +253,22 @@ function do_k3s_install {
 
 function install_k8s_tools { 
     printf "==> install kubernetes tools, kubens, kubectx kustomize \n" 
-    curl -s -L https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz | gzip -d -c | tar xf -
-    mv ./kubens /usr/local/bin
+    curl -s -L https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz| gzip -d -c | tar xf - 
+    mv ./kubens /usr/local/bin > /dev/null 2>&1
     curl -s -L https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz | gzip -d -c | tar xf -
-    mv ./kubectx /usr/local/bin
-
+    mv ./kubectx /usr/local/bin > /dev/null
+    
     # install kustomize
-    curl -s "https://raw.githubusercontent.com/\
-    kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-    mv ./kustomize /usr/local/bin
+    curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+    mv ./kustomize /usr/local/bin > /dev/null 2>&1
 }
 
 function add_helm_repos { 
     printf "==> add the helm repos required to install and run Mojaloop version 13.x \n" 
-    su - $k8s_user -c "helm repo add kiwigrid https://kiwigrid.github.io"
-    su - $k8s_user -c "helm repo add elastic https://helm.elastic.co"
-    su - $k8s_user -c "helm repo add bitnami https://charts.bitnami.com/bitnami"
-    su - $k8s_user -c "helm repo update"
+    su - $k8s_user -c "helm repo add kiwigrid https://kiwigrid.github.io" > /dev/null 2>&1
+    su - $k8s_user -c "helm repo add elastic https://helm.elastic.co" > /dev/null 2>&1
+    su - $k8s_user -c "helm repo add bitnami https://charts.bitnami.com/bitnami" > /dev/null 2>&1
+    su - $k8s_user -c "helm repo update" > /dev/null 2>&1
 }
 
 function configure_k8s_user_env { 
@@ -262,6 +305,31 @@ function verify_user {
             printf "** \n"
             exit 1 
         fi    
+}
+
+function remove_k8s {
+    if [[ "$k8s_distro" == "microk8s" ]]; then 
+        printf "==>Removing any existing Microk8s installation "
+        snap remove microk8s > /dev/null 2>&1
+        if [[ $? -eq 0  ]]; then 
+            printf " [ ok ] \n"
+        else 
+            printf " [ microk8s remove failed ] \n"
+            printf "** was microk8s installed ?? \n" 
+            printf "   if so please try running \"snap remove microk8s\" manually ** \n"
+        fi
+    else 
+        printf "==>Removing any existing k3s installation "
+        /usr/local/bin/k3s-uninstall.sh >> /dev/null 2>&1
+        if [[ $? -eq 0  ]]; then 
+            printf " [ ok ] \n"
+        else 
+            printf " [ k3s remove failed ] \n"
+            printf "** was k3s installed ?? \n" 
+            printf "   if so please try running \"/usr/local/bin/k3s-uninstall.sh\" manually ** \n"
+        fi
+        exit
+    fi     
 }
 
 function check_k8s_installed { 
@@ -323,6 +391,10 @@ OS_VERSIONS_LIST=(16 18 20 )
 k8s_user_home=""
 k8s_arch=`uname -p`  # what arch
 
+UBUNTU_OK_VERSIONS_LIST=(16 18 20 )
+FEDORA_OK_VERSIONS_LIST=( 36 )
+REDHAT_OK_VERSIONS_LIST=( 8 )
+
 # ensure we are running as root 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -366,9 +438,11 @@ if [[ "$mode" == "install" ]]  ; then
     
     check_pi  # note microk8s on my pi still has some issues around cgroups 
     ## when I have k3s going => only need to check OS if using microk8s !
-    check_os_ok # check this is an ubuntu OS v18.04 or later 
+    if [[ "$k8s_distro" == "microk8s" ]]; then 
+        check_os_ok # check this is an ubuntu OS v18.04 or later 
+    fi 
     verify_user 
-    #install_prerequisites 
+    install_prerequisites 
     set_k8s_distro
     set_k8s_version
     add_hosts
@@ -380,33 +454,12 @@ if [[ "$mode" == "install" ]]  ; then
     install_k8s_tools
     add_helm_repos 
     configure_k8s_user_env
+    check_k8s_installed
     printf "==> The kubernetes environment is now configured for user [%s] and ready for mojaloop deployment \n" "$k8s_user"
     printf "    To deploy mojaloop, please su - %s from root  or login as user [%s] and then \n"  "$k8s_user" "$k8s_user"
     printf "    execute the %s/01_install_miniloop.sh script \n"  "$SCRIPTS_DIR"    
 elif [[ "$mode" == "remove" ]]  ; then
-    
-    if [[ "$k8s_distro" == "microk8s" ]]; then 
-        printf "==>Removing any existing Microk8s installation "
-        snap remove microk8s > /dev/null 2>&1
-        if [[ $? -eq 0  ]]; then 
-            printf " [ ok ] \n"
-        else 
-            printf " [ microk8s remove failed ] \n"
-            printf "** was microk8s installed ?? \n" 
-            printf "   if so please try running \"snap remove microk8s\" manually ** \n"
-        fi
-    else 
-        printf "==>Removing any existing k3s installation "
-        /usr/local/bin/k3s-uninstall.sh >> /dev/null 2>&1
-        if [[ $? -eq 0  ]]; then 
-            printf " [ ok ] \n"
-        else 
-            printf " [ k3s remove failed ] \n"
-            printf "** was k3s installed ?? \n" 
-            printf "   if so please try running \"/usr/local/bin/k3s-uninstall.sh\" manually ** \n"
-        fi
-        exit
-    fi 
+    remove_k8s 
 else 
     showUsage
 fi 
