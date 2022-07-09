d5673df223035ebecbd9e7eb1e4dbb72f802e936
tdaly61 <tdaly61@gmail.com>
2022-07-09 13:16:21 +0000

    install_local_miniloop.sh now sucessfully deploying ML to v1.24

--- install/mini-loop/scripts/install_local_miniloop.sh
+++ install/mini-loop/scripts/install_local_miniloop.sh
@@ -90,59 +90,7 @@ function set_and_create_namespace {
 #   printf " ==> Using the values file [%s] \n" "$ETC_DIR/$ML_VALUES_FILE"
 # }
 
-function deploy_mojaloop_from_local {
-  # uninstall the old chart if it exists
-  printf " ==> uninstalling any previous mojaloop deployment "
-  helm uninstall ${RELEASE_NAME} --namespace "$NAMESPACE"  >/dev/null 2>&1
-  printf "  [ok] \n\n"
-
-  # install the chart
-  printf  " ==> install %s helm chart and wait for upto %s  secs for it to be ready \n" "$RELEASE_NAME" "$TIMEOUT_SECS"
-  printf  "     executing helm install $RELEASE_NAME --wait --timeout $TIMEOUT_SECS $HOME/charts/mojaloop  \n "
-  helm install $RELEASE_NAME --wait --timeout $TIMEOUT_SECS  --namespace "$NAMESPACE" $HOME/charts/mojaloop  
-
-  if [[ `helm status $RELEASE_NAME  --namespace "$NAMESPACE" | grep "^STATUS:" | awk '{ print $2 }' ` = "deployed" ]] ; then 
-    printf " ==> [%s] deployed sucessfully \n" "$RELEASE_NAME"
-  else 
-    printf "** Error: %s helm chart deployment failed \n" "$RELEASE_NAME"
-    printf "   Possible reasons include : - \n"
-    printf "     very slow internet connection /  issues downloading container images (e.g. docker rate limiting) \n"
-    printf "     slow machine/vm instance / insufficient memory to start all pods (6GB = min  , 8GB = preferred ) \n"
-    printf "**\n\n"
-
-    printf "The current timeout for all pods to be ready is %s \n" "$TIMEOUT_SECS"
-    printf "** Possible actions \n"
-    printf "   1) allow the deployment to run a little longer , you an check on progress by running kubectl get pods \n"
-    printf "      and examining to see if pods are still reaching \"running\" state over the next 10-20 mins \n"
-    printf "      If all the pods do reach running state you can then run the helm tests by executing \n"
-    printf "      helm -n %s test ml --logs \n" "$NAMESPACE"  
-
-    printf "   2) You can re-run this script with a timeout value longer than the default %s secs \n" "$DEFAULT_TIMEOUT_SECS"
-    printf "       use the -t timeout_secs parameter or run %s -h for example(s) \n" "$0"
-    printf "**\n\n"
-    exit 1
-  fi 
-}
-
-function check_deployment_health {
-  # verify the health of the deployment 
-  # curl to http://ml-api-adapter.local/health and http://central-ledger.local/health
-  # TODO: what should we suggest if the endpoints are not working and yet the deployment succeeds ? 
-  #       ideally this should never happen from the mini-loop install (ideally)
-  if [[ `curl -s http://central-ledger.local/health | \
-      perl -nle '$count++ while /OK+/g; END {print $count}' ` -lt 3 ]] ; then
-      printf  " ** Error: central-leger endpoint healthcheck failed ** \n"
-      exit 1
-  fi
-  if [[ `curl -s http://ml-api-adapter.local/health | \
-      perl -nle '$count++ while /OK+/g; END {print $count}' ` -lt 2 ]] ; then
-      printf  " ** Error: ml-api-adapter endpoint healthcheck failed ** \n"
-      exit 1 
-  fi
-}
-
-
-function clone_and_modify_helm_charts { 
+function clone_helm_charts_repo { 
   printf "==> cloning mojaloop helm charts repo  "
   if [ ! -z "$force" ]; then 
     #printf "==> removing existing helm directory\n"
@@ -150,10 +98,20 @@ function clone_and_modify_helm_charts {
   fi 
   if [ ! -d $HOME/helm ]; then 
     git clone https://github.com/mojaloop/helm.git $HOME/helm > /dev/null 2>&1
-    # note: this also updates $ETC_DIR/mysql_values.yaml with a new DB password
-    $SCRIPTS_DIR/mod_local_miniloop.py -d $HOME/helm -i > /dev/null 2>&1
-    NEED_TO_REPACKAGE="true"
+    printf " [ done ] \n"
+  else 
+    printf "\n ** INFO: helm repo is not cloned as there is an existing $HOME/helm directory\n"
+    printf "      to get a fresh clone of the repo , either delete $HOME/helm of use the -f flag **\n"
   fi
+  
+}
+
+function modify_local_helm_charts {
+  printf "==> modifying the local mojaloop helm charts to run on kubernetes v1.22+  "
+  # note: this also updates $ETC_DIR/mysql_values.yaml with a new DB password
+  # this password is and needs to be the same in all the values files which access the DB
+  $SCRIPTS_DIR/mod_local_miniloop.py -d $HOME/helm -i > /dev/null 2>&1
+  NEED_TO_REPACKAGE="true"
   printf " [ done ] \n"
 }
 
@@ -202,14 +160,17 @@ function delete_db {
 }
 
 function install_db { 
-  db_exists=`helm ls | grep $DB_RELEASE_NAME | cut -d " " -f1`
-  # if the database is already deployed please get the user to delete it first
-  # TODO do the delete automatically if the force flag is set
-  if [ ! -z $db_exists ] && [ "$db_exists" == "$DB_RELEASE_NAME" ]; then 
-    printf "** Error: the mojaloop database is already installed please delete before re-install \n" 
-    printf "   you can use install_local_miniloop.sh -m delete_db to do this cleanly ** \n"
-    exit 1
-  fi
+  # db_exists=`helm ls | grep $DB_RELEASE_NAME | cut -d " " -f1`
+  # # if the database is already deployed please get the user to delete it first
+  # # TODO do the delete automatically if the force flag is set
+  # if [ ! -z $db_exists ] && [ "$db_exists" == "$DB_RELEASE_NAME" ]; then 
+  #   printf "** Error: the mojaloop database is already installed please delete before re-install \n" 
+  #   printf "   you can use install_local_miniloop.sh -m delete_db to do this cleanly ** \n"
+  #   exit 1
+  # fi
+
+  # delete  db cleanly if it is already deployed => so we can confifdently reinstall cleanly 
+  delete_db
 
   # deploy the bitnami mysql database chart
   printf "==> deploying mojaloop database from bitnami helm chart, waiting upto 300s for it to be ready  \n"
@@ -221,6 +182,56 @@ function install_db {
   fi 
 }
 
+function deploy_mojaloop_from_local {
+  # uninstall the old chart if it exists
+  printf " ==> uninstalling any previous mojaloop deployment "
+  helm uninstall ${RELEASE_NAME} --namespace "$NAMESPACE"  >/dev/null 2>&1
+  printf "  [ok] \n\n"
+
+  # install the chart
+  printf  " ==> install %s helm chart and wait for upto %s  secs for it to be ready \n" "$ML_RELEASE_NAME" "$TIMEOUT_SECS"
+  printf  "     executing helm install $RELEASE_NAME --wait --timeout $TIMEOUT_SECS $HOME/helm/mojaloop  \n "
+  helm install $ML_RELEASE_NAME --wait --timeout $TIMEOUT_SECS  --namespace "$NAMESPACE" $HOME/helm/mojaloop  
+
+  if [[ `helm status $ML_RELEASE_NAME  --namespace "$NAMESPACE" | grep "^STATUS:" | awk '{ print $2 }' ` = "deployed" ]] ; then 
+    printf " ==> [%s] deployed sucessfully \n" "$ML_RELEASE_NAME"
+  else 
+    printf "** Error: %s helm chart deployment failed \n" "$ML_RELEASE_NAME"
+    printf "   Possible reasons include : - \n"
+    printf "     very slow internet connection /  issues downloading container images (e.g. docker rate limiting) \n"
+    printf "     slow machine/vm instance / insufficient memory to start all pods (6GB = min  , 8GB = preferred ) \n"
+    printf "**\n\n"
+
+    printf "The current timeout for all pods to be ready is %s \n" "$TIMEOUT_SECS"
+    printf "** Possible actions \n"
+    printf "   1) allow the deployment to run a little longer , you an check on progress by running kubectl get pods \n"
+    printf "      and examining to see if pods are still reaching \"running\" state over the next 10-20 mins \n"
+    printf "      If all the pods do reach running state you can then run the helm tests by executing \n"
+    printf "      helm -n %s test ml --logs \n" "$NAMESPACE"  
+
+    printf "   2) You can re-run this script with a timeout value longer than the default %s secs \n" "$DEFAULT_TIMEOUT_SECS"
+    printf "       use the -t timeout_secs parameter or run %s -h for example(s) \n" "$0"
+    printf "**\n\n"
+    exit 1
+  fi 
+}
+
+function check_deployment_health {
+  # verify the health of the deployment 
+  # curl to http://ml-api-adapter.local/health and http://central-ledger.local/health
+  # TODO: what should we suggest if the endpoints are not working and yet the deployment succeeds ? 
+  #       ideally this should never happen from the mini-loop install (ideally)
+  if [[ `curl -s http://central-ledger.local/health | \
+      perl -nle '$count++ while /OK+/g; END {print $count}' ` -lt 3 ]] ; then
+      printf  " ** Error: central-leger endpoint healthcheck failed ** \n"
+      exit 1
+  fi
+  if [[ `curl -s http://ml-api-adapter.local/health | \
+      perl -nle '$count++ while /OK+/g; END {print $count}' ` -lt 2 ]] ; then
+      printf  " ** Error: ml-api-adapter endpoint healthcheck failed ** \n"
+      exit 1 
+  fi
+}
 
 ################################################################################
 # Function: showUsage
@@ -234,7 +245,7 @@ function showUsage {
 		echo "Incorrect number of arguments passed to function $0"
 		exit 1
 	else
-echo  "USAGE: $0 -m <mode> [-t secs] [-n namespace] [-f] [-h] 
+echo  "USAGE: $0 -m <mode> [-t secs] [-n namespace] [-f] [-h] [-s] 
 Example 1 : $0 -m install_ml -t 3000 # install mojaloop using a timeout of 3000 seconds 
 Example 2 : $0 -m install_ml -n moja # create namespace moja and deploy mojaloop into the moja namespace 
 Example 3 : $0 -m install_db         # install the mojaloop database only (no mojaloop install)
@@ -242,6 +253,7 @@ Example 4 : $0 -m delete_db          # cleanly delete the mojaloop database only
  
 Options:
 -m mode ............ install_ml|install_db|delete_db 
+-s skip_repackage .. mainly for test/dev use (skips the repackage of the local charts)
 -t secs ............ number of seconds (timeout) to wait for pods to all be reach running state
 -n namespace ....... the namespace to deploy mojaloop into 
 -f force ........... force the cloning and updating of the helm charts (will destory existing $HOME/helm)
@@ -270,7 +282,7 @@ NEED_TO_REPACKAGE="false"
 #ML_VALUES_FILE="miniloop_values.yaml"
 
 # Process command line options as required
-while getopts "dft:n:m:hH" OPTION ; do
+while getopts "dfst:n:m:hH" OPTION ; do
    case "${OPTION}" in
         f)  force="true"
         ;; 
@@ -281,6 +293,8 @@ while getopts "dft:n:m:hH" OPTION ; do
         d)  devmode="true"
         ;; 
         m)  mode="${OPTARG}"
+        ;;
+        s)  skip_repackage="true"
         ;;
         h|H)	showUsage
                 exit 0
@@ -293,7 +307,7 @@ while getopts "dft:n:m:hH" OPTION ; do
 done
 
 printf "\n\n**********************************************************************************************\n"
-printf " Mojaloop.io mini-loop deploying local Mojaloop helm chart for kubernetes 1.22+ >>>  start        \n"
+printf " Mojaloop.io mini-loop deploying modified local Mojaloop helm chart for kubernetes 1.22+ >>>  start        \n"
 printf "************************************************************************************************\n\n"
 check_arch
 check_user
@@ -307,12 +321,19 @@ if [[ "$mode" == "install_db" ]]; then
 elif [[ "$mode" == "delete_db" ]]; then
   delete_db
 elif [[ "$mode" == "install_ml" ]]; then
-  clone_and_modify_helm_charts
-  repackage_charts
+  clone_helm_charts_repo
+  modify_local_helm_charts
+  if [ -z skip_repackage ]; then 
+    repackage_charts
+  fi
   install_db
   #set_mojaloop_values_file
   deploy_mojaloop_from_local
+else 
+  printf "** Error : wrong value for -m ** \n\n"
 
+  showUsage
+  exit 1
 fi 
 
 check_deployment_health
@@ -336,5 +357,5 @@ printf "        With this caution in mind , welcome to the full function of Moja
 printf "        please see : https://mojaloop.io/ for more information, resources and online training\n"
 
 printf "\n****************************************************************************************************************\n"
-printf " Mojaloop.io mini-loop deploying local Mojaloop helm chart for kubernetes 1.22+ >>>  start       >>> end       \n"
+printf " Mojaloop.io mini-loop deploying local Mojaloop helm chart for kubernetes 1.22+ >>>  end       \n"
 printf "****************************************************************************************************************\n\n"
