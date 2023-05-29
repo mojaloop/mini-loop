058e9777b5d8d9baad50baef5354402f0be410af
Tom Daly <tdaly61@gmail.com>
2023-05-29 10:59:47 +0000

    updating sundry scripts

--- util/sundry/apply-or-delete-yaml.sh
+++ util/sundry/apply-or-delete-yaml.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
 # Get the list of YAML files in the current directory except for files beginning with "docker-"
-yaml_files=$(ls *.yml *.yaml | grep -v '^docker-')
+yaml_non_dataresource_files=$(ls *.yml *.yaml | grep -v '^docker-' | grep -v "\-data\-" )
+yaml_dataresource_files=$(ls *.yml *.yaml | grep -v '^docker-' | grep -i "\-data\-" )
 
-# Apply or delete the YAML files using kubectl
-for file in $yaml_files; do
-    if [[ $1 == "apply" ]]; then
+if [[ $1 == "apply" ]]; then
+    for file in $yaml_dataresource_files; do
         kubectl apply -f $file
-    elif [[ $1 == "delete" ]]; then
+    done
+    for file in $yaml_non_dataresource_files; do
+        kubectl apply -f $file
+    done
+elif [[ $1 == "delete" ]]; then
+    for file in $yaml_non_dataresource_files; do
         kubectl delete -f $file
-    else
-        echo "Usage: $0 (apply|delete)"
-        exit 1
-    fi
-done
+    done
+    for file in $yaml_dataresource_files; do
+        kubectl delete -f $file
+    done
+else 
+    echo "Usage: $0 (apply|delete)"
+    exit 1
+fi
+
+
+# # Apply or delete the YAML files using kubectl
+# for file in $yaml_files; do
+#     if [[ $1 == "apply" ]]; then
+#         kubectl apply -f $file
+#     elif [[ $1 == "delete" ]]; then
+#         kubectl delete -f $file
+#     else
+#         echo "Usage: $0 (apply|delete)"
+#         exit 1
+#     fi
+# done
+
