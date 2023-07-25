509554af28f1ba6dbc6cee556f1da0d156760756
Elijah Okello <elijahokello90@gmail.com>
2023-07-25 10:35:44 +0000

    setup initial scripts for fineract

---
+++ scripts/fineract/fineractInstall.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+source 
+
+#####################################################################
+# Steps to install fineract
+######################################################################
+# 1. Clone the fineract helm chart repository
+# 2. Build the helm dependencies
+# 3. Install the helm chart
+#
+#
+
+clone_fineract_repo{
+    
+}
+
+build_helm_dependencies{
+
+}
+
+install_a_fineract_release{
+
+}
